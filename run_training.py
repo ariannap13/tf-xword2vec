26c885e26b538cf5b9f778ad2e4931686460361f
eduamf <eduardo.freitas@camara.leg.br>
2020-06-20 14:07:29 +0100

    Update

--- run_training.py
+++ run_training.py
@@ -23,11 +23,11 @@ from word2vec import Word2VecModel
 
 flags = tf.app.flags
 
-flags.DEFINE_string('arch', 'cbow', 'Architecture (skip_gram or cbow).')
-flags.DEFINE_string('algm', 'hierarchical_softmax', 'Training algorithm '
+flags.DEFINE_string('arch', 'skip_gram', 'Architecture (skip_gram or cbow).')
+flags.DEFINE_string('algm', 'negative_sampling', 'Training algorithm '
     '(negative_sampling or hierarchical_softmax).')
-flags.DEFINE_integer('epochs', 15, 'Num of epochs to iterate training data.')
-flags.DEFINE_integer('batch_size', 256, 'Batch size.')
+flags.DEFINE_integer('epochs', 20, 'Num of epochs to iterate training data.')
+flags.DEFINE_integer('batch_size', 200, 'Batch size.')
 flags.DEFINE_integer('max_vocab_size', 0, 'Maximum vocabulary size. '
                      'If > 0, the top `max_vocab_size` most frequent words'
                      ' are kept in vocabulary.')
@@ -36,11 +36,11 @@ flags.DEFINE_integer('min_count', 2, 'Words whose counts < `min_count` are not'
 flags.DEFINE_float('sample', 0.01, 'Subsampling rate.')
 flags.DEFINE_integer('window_size', 6, 'Num of words on the left or right side' 
                                        ' of target word within a window.')
-flags.DEFINE_integer('embed_size', 200, 'Length of word vector.')
+flags.DEFINE_integer('embed_size', 100, 'Length of word vector.')
 flags.DEFINE_integer('negatives', 5, 'Num of negative words to sample.')
 flags.DEFINE_float('power', 0.75, 'Distortion for negative sampling.')
-flags.DEFINE_float('alpha', 0.025, 'Initial learning rate to Gradient Descent.')
-flags.DEFINE_float('min_alpha', 0.001, 'Final learning rate.')
+flags.DEFINE_float('alpha', 0.050, 'Initial learning rate to Gradient Descent.')
+flags.DEFINE_float('min_alpha', 0.002, 'Final learning rate.')
 flags.DEFINE_boolean('add_bias', True, 'Whether to add bias term to dotproduct'
                                        ' between syn0 and syn1 vectors.')
 flags.DEFINE_integer('log_per_steps', 1000, 'Every `log_per_steps` steps to '
@@ -51,7 +51,7 @@ flags.DEFINE_integer('seed', 777, 'Seed to fix sequence of random values.')
 flags.DEFINE_string('optim', 'GradDesc', 'Optimization algorithm '
                             '(GradDescProx, GradDesc, Adam, AdaGradProx).')
 flags.DEFINE_string('decay', 'cos', 'Polynomial (poly), cosine (cos) or (no).')
-flags.DEFINE_boolean('less_first', True, 'First epoch with less log.')
+flags.DEFINE_boolean('wait_first', True, 'First epoch with no log.')
 
 FLAGS = flags.FLAGS
 
@@ -101,11 +101,12 @@ def main(_):
 
     print("optimizer: ", FLAGS.optim)
     print("epochs: ", FLAGS.epochs)
+    print("model: ", FLAGS.arch)
     
     # open log file
     log_arq = "log_" + FLAGS.optim + "_" + FLAGS.arch + time_sufix(".log")
     flog = open(os.path.join(curr_path, FLAGS.out_dir, log_arq), "w", encoding="utf-8")
-    flog.write("Step\tEpoch\tLoss")
+    flog.write("Step\tEpoch\tAverageLoss\tLearningRate")
 
     average_loss = 0.
     step = 0
@@ -122,46 +123,49 @@ def main(_):
       step += 1
       sub_step += 1
       average_loss += result_dict['loss'].mean()
+      op_lr = result_dict['learning_rate']
+      op_epoch = result_dict['epoch']
       
-      if result_dict['epoch'][0] > nepoch:
-        syn0_partial = sess.run(word2vec.syn0)
-        np.save(os.path.join(FLAGS.out_dir, 'embed_' + 
-                             str(nepoch).zfill(2)), syn0_partial)
-        nepoch = result_dict['epoch'][0]
-        print('-------------------- Epoch: ', nepoch)
-      
-      if nepoch > 1:
-        if FLAGS.less_first:
-          divisor = FLAGS.log_per_steps / 2
+      if step == 1:
+          # first one
+          syn0_partial = sess.run(word2vec.syn0)
+          np.save(os.path.join(FLAGS.out_dir, 'embed_' + 
+                               str(nepoch).zfill(2)), syn0_partial)
+          print('-------------------- Epoch: ', op_epoch)
+          print(' average loss:', average_loss / sub_step)
+          print(' learning rate:', op_lr)
+          print('------------------------------------')
+          flog.write("\n" + str(step) + "\t" + str(op_epoch) \
+                     + "\t" + str(average_loss / sub_step) \
+                     + "\t" + str(op_lr))
       else:
         divisor = FLAGS.log_per_steps
 
-      if step % divisor == 0:
-        average_loss /= FLAGS.log_per_steps
-        print('epoch:', nepoch, ' step:', step)
-        print(' average loss:', average_loss)
-        print(' learning rate:', result_dict['learning_rate'])
-        print('------------------------------------')
-        flog.write("\n" + str(step) + "\t" + str(nepoch) \
-                                    + "\t" + str(average_loss))
-
-        syn0_partial = sess.run(word2vec.syn0)
-        np.save(os.path.join(FLAGS.out_dir, 'embed_' + 
-                             str(nepoch).zfill(2) + "_step_" +
-                             str(step).zfill(6))
-                , syn0_partial)
-        average_loss = 0.
-        sub_step = 0
-        no_log = False
+        if step % divisor == 0:
+          print('epoch:', op_epoch, ' step:', step)
+          print(' average loss:', average_loss / sub_step)
+          print(' learning rate:', op_lr)
+          print('------------------------------------')
+          flog.write("\n" + str(step) + "\t" + str(op_epoch) \
+                          + "\t" + str(average_loss / sub_step)
+                          + "\t" + str(op_lr))
+  
+          syn0_partial = sess.run(word2vec.syn0)
+          np.save(os.path.join(FLAGS.out_dir, 'embed_' + 
+                               str(nepoch).zfill(2) + "_step_" +
+                               str(step).zfill(6)), syn0_partial)
+          average_loss = 0.
+          sub_step = 0
+          no_log = False
       
     if no_log:
       if sub_step > 0:
         average_loss /= sub_step
-        print('epoch:', nepoch, ' step:', step) 
+        print('epoch:', op_epoch, ' step:', step) 
         print(' average loss:', average_loss)
         print(' learning rate:', result_dict['learning_rate'])
         
-        flog.write("\n" + str(step) + "\t" + str(nepoch) \
+        flog.write("\n" + str(step) + "\t" + str(op_epoch) \
                                     + "\t" + str(average_loss))
     flog.close()
     
@@ -173,7 +177,7 @@ def main(_):
         fid.write(w + '\n')       
       fid.close()
 
-    with open(os.path.join(FLAGS.out_dir, 'vocab.txt'), 'w', encoding="utf-8") as fid:
+    with open(os.path.join(FLAGS.out_dir, 'vocab_counts.txt'), 'w', encoding="utf-8") as fid:
       for c in dataset.unigram_counts:
         fid.write(str(c) + '\n')       
       fid.close()
